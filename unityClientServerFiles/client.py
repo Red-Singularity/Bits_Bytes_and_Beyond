b68b4f64f43ec6947ad291d9a1207a9909834d84
Red-Singularity <dusfisher@gmail.com>
2023-04-28 17:21:25 -0400

    client test

--- unityClientServerFiles/client.py
+++ unityClientServerFiles/client.py
@@ -6,12 +6,17 @@ A simple echo client
 
 import socket 
 
-host = '127.0.0.1'
-port = 55002 
-size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((host,port)) 
-s.send('Hello, world') 
-data = s.recv(size) 
-s.close() 
-print 'Received:', data
+def main():
+    print("starting client")
+    host = '192.168.1.4'
+    port = 55002 
+    size = 1024 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.connect((host,port)) 
+    s.send('Hello, world') 
+    data = s.recv(size) 
+    s.close() 
+    print ('Received:'), data
+
+if __name__ == "__main__": 
+    main()
--- unityClientServerFiles/server.py
+++ unityClientServerFiles/server.py
@@ -6,17 +6,23 @@ A simple echo server
 
 import socket 
 
-host = '192.168.0.11' 
-port = 55002 
-backlog = 5 
-size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.bind((host,port)) 
-s.listen(backlog) 
-while 1: 
-    client, address = s.accept() 
-    print "connection made"
-    data = client.recv(size) 
-    if data: 
-        client.send(data) 
-    client.close()
+def main():
+    print("starting")
+    host = '192.168.1.4' 
+    port = 55002 
+    backlog = 5 
+    size = 1024 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.bind((host,port)) 
+    print("waitining for connection")
+    s.listen(backlog) 
+    while 1: 
+        client, address = s.accept() 
+        print ("connection made")
+        data = client.recv(size) 
+        if data: 
+            client.send(data) 
+        client.close()
+
+if __name__ == "__main__": 
+    main()
